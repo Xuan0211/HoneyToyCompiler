5943a8b9282c77928f596436062e570115ee9f15
Xuan0211 <2022134346@qq.com>
2023-06-04 07:31:35 -0700

    second commit

--- htc.c
+++ htc.c
@@ -1,1 +1,28 @@
-﻿
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <stdint.h>
+#ifndef _MSC_VER
+#include <unistd.h>
+#endif
+
+#if (defined _MSC_VER) && (defined _WIN64) || (defined __LP64__)
+#define int int64_t
+#endif
+
+
+// VM
+char *data;         // 数据段
+int *code,          // 代码段
+    *stack;         // 运行栈
+int *pc, *sp, *bp, ax, cycle; // 寄存器
+int poolsize;       // 各个段分配内存大小
+
+
+int main(int argc, char** argv)
+{
+	int tmp;
+	int fd;
+	printf("hello word");
+	return 0;
+}
